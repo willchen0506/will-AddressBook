da30c2daef89d52145c1a8b1917c9494b5ab02be
Will Chen <willchen0506@gmail.com>
2018-03-17 20:43:46 -0700

    added user account

--- src/server/index.js
+++ src/server/index.js
@@ -1,13 +1,28 @@
 const Koa = require('koa');
 const bodyParser = require('koa-bodyparser');
+const session = require('koa-session');
+const passport = require('koa-passport');
 
 const indexRoutes = require('./routes/index');
 const contactRoutes = require('./routes/contacts');
+const authRoutes = require('./routes/auth');
 
 const app = new Koa();
 const PORT = process.env.PORT || 1337;
 
+// sessions
+app.keys = ['will-secret-key'];
+app.use(session(app));
+
+// body parser
 app.use(bodyParser());
+
+// authentication
+require('./auth');
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(authRoutes.routes());
+
 app.use(indexRoutes.routes());
 app.use(contactRoutes.routes());
 
